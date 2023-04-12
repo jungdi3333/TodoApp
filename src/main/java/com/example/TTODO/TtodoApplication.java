65fbec61f677591d6482d3f8e3413785368db95f
jungdi3333 <jungdi3333@google.com>
2023-04-12 21:18:34 +0900

    todotodo

---
+++ src/main/java/com/example/TTODO/TtodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.TTODO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TTODO/TtodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TTODO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
